cb2cb8ffe4726b526cfdc515cf8998a45f7e2817
ixaxaar <akairyu>
2013-08-05 01:58:14 +0530

    [Russi]: Winterfell ongoing development

--- modules/winterfell/lib/feed.js
+++ modules/winterfell/lib/feed.js
@@ -1,6 +1,43 @@
 
 // schema library
 var mongoose = require('mongoose');
+var uuid = require('node-uuid');
+
+////////////////////////////////
+//   Child Feed Schema
+////////////////////////////////
+
+// child feed schema
+var ChildFeedSchema = new Schema({
+    owner: String, // the owner's _id
+    created: { type : Date, default: Date.now },
+    updated: { type : Date, default: Date.now },
+    content: String // the content object's _id
+});
+
+ChildFeedSchema.methods.Save = function(user, content) {
+    this.created = Date.now;
+    this.owner = user._id;
+    this.content = content;
+    this.next = null;
+    this.save(function(err, cf) {
+    });
+};
+
+ChildFeedSchema.methods.ModifyContent = function(content, fn) {
+    this.content = content;
+    this.modified = Date.now;
+    this.save(function(err, f) {
+        if (err || !f) fn('Could not modify content', null);
+        else fn(null, f);
+    });
+};
+
+ChildFeedSchema.methods.Delete = function(fn) {
+    this.remove(fn);
+};
+
+var ChildFeed = mongoose.model("ChildFeedSchema", ChildFeedSchema);
 
 
 ////////////////////////////////
@@ -9,143 +46,171 @@ var mongoose = require('mongoose');
 
 // feed schema
 var FeedSchema = new Schema({
-	owner: String, // the owner's _id
-	created: { type : Date, default: Date.now },
-	updated: { type : Date, default: Date.now },
-	content: String, // the content object's _id
-	tags: [String], // tags to group similar feeds
-	acl: [String] // list of people having access to this
-	children: [String] // linked-list of child feeds
+    uuid: { type: String, default: uuid.v4() }, // this is what is served, not the _id
+    owner: String, // the owner's _id
+    created: { type : Date, default: Date.now },
+    updated: { type : Date, default: Date.now },
+    content: String, // the content object's _id
+    tags: [String], // tags to group similar feeds
+    acl: [String], // list of people having access to this
+    children: [ChildFeedSchema] // linked-list of child feeds
 });
 
-FeedSchema.methods.save = function(user, content, tagList, fn) {
-	this.created = Date.now;
-	this.owner = user._id;
-	this.content = content;
-	this.children = [];
+FeedSchema.methods.Save = function(user, content, tagList, fn) {
+    this.created = Date.now;
+    this.owner = user._id;
+    this.content = content;
+    this.children = [];
 
-	that = this;
-	if (tagList.length > 1) {
-		tagList.forEach(function(t) {
-			that.tags.push(t);
-		});
-	}
+    var that = this;
+    if (tagList.length > 1) {
+        tagList.forEach(function(t) {
+            that.tags.push(t);
+        });
+    }
 
-	// 'updated' gets autoupdated here
-	this.save(function(err, t) {
-		if (!err || t) fn(null, t);
-		else fn('Could not save', null);
-	});
+    // 'updated' gets autoupdated here
+    this.save(function(err, t) {
+        if (!err || t) fn(null, t);
+        else fn('Could not save', null);
+    });
 };
 
-FeedSchema.methods.modifyContent = function(content, fn) {
-	// modify content
-	this.content = content;
-	this.updated = Date.now;
-	this.save(function(err, f) {
-		if (err || !f) fn('Could not modify content', null);
-		else fn(null, f);
-	});
+FeedSchema.methods.ModifyContent = function(content, fn) {
+    // modify content
+    this.content = content;
+    this.updated = Date.now;
+    this.save(function(err, f) {
+        if (err || !f) fn('Could not modify content', null);
+        else fn(null, f);
+    });
 };
 
-FeedSchema.methods.addTag = function(tag, fn) {
-	// append to the access control list
-	if (this.tags.push(tag)) {
-		this.updated = Date.now;
-		this.save(function(err, u) {
-			if (err) fn(err, null);
-			else fn(null, u);
-		});
-	} else fn('Could not grant tag', null);
+FeedSchema.methods.AddTag = function(tag, fn) {
+    // append to the access control list
+    if (this.tags.push(tag)) {
+        this.updated = Date.now;
+        this.save(function(err, u) {
+            if (err) fn(err, null);
+            else fn(null, u);
+        });
+    } else fn('Could not grant tag', null);
 };
 
-FeedSchema.methods.removeTag = function(tag, fn) {
-	// append to the access control list
-	if (this.tags.remove(tag)) {
-		this.updated = Date.now;
-		this.save(function(err, u) {
-			if (err) fn(err, null);
-			else fn(null, u);
-		});
-	} else fn('Could not remove tag', null);
+FeedSchema.methods.RemoveTag = function(tag, fn) {
+    // append to the access control list
+    if (this.tags.remove(tag)) {
+        this.updated = Date.now;
+        this.save(function(err, u) {
+            if (err) fn(err, null);
+            else fn(null, u);
+        });
+    } else fn('Could not remove tag', null);
 };
 
-FeedSchema.methods.grantAccess = function(user, fn) {
-	// append to the access control list
-	if (this.acl.push(user)) {
-		this.updated = Date.now;
-		this.save(function(err, u) {
-			if (err) fn(err, null);
-			else fn(null, u);
-		});
-	} else fn('Could not grant permission', null);
+FeedSchema.methods.GrantAccess = function(user, fn) {
+    // append to the access control list
+    if (this.acl.push(user)) {
+        this.updated = Date.now;
+        this.save(function(err, u) {
+            if (err) fn(err, null);
+            else fn(null, u);
+        });
+    } else fn('Could not grant permission', null);
 };
 
-FeedSchema.methods.revokeAccess = function(user, fn) {
-	// append to the access control list
-	if (this.acl.remove(user)) {
-		this.updated = Date.now;
-		this.save(function(err, u) {
-			if (err) fn(err, null);
-			else fn(null, u);
-		});
-	} else fn('Could not remove permission', null);
+FeedSchema.methods.RevokeAccess = function(user, fn) {
+    // append to the access control list
+    if (this.acl.remove(user)) {
+        this.updated = Date.now;
+        this.save(function(err, u) {
+            if (err) fn(err, null);
+            else fn(null, u);
+        });
+    } else fn('Could not remove permission', null);
 };
 
-FeedSchema.methods.delete = function(fn) {
-	this.remove(fn);
+FeedSchema.methods.Delete = function(fn) {
+    this.remove(fn);
 };
 
-FeedSchema.methods.addChild = function(child, fn) {
-	this.children.push(child);
-	this.modified = Date.now;
-	this.save(function(err, f) {
-		if (!err || f) fn(null, f);
-		else fn('Could not add child feed', null);
-	});
+FeedSchema.methods.AddChild = function(user, content, fn) {
+    var c = new ChildFeed({});
+    c.owner = user._id;
+    c.created = Date.now;
+    c.content = content;
+
+    this.children.push(child);
+    this.modified = Date.now;
+    this.save(function(err, f) {
+        if (!err || f) fn(null, f);
+        else fn('Could not add child feed', null);
+    });
 };
 
 var Feed = mongoose.model("FeedSchema", FeedSchema);
+//exports.Feed = Feed;
 
+var findFeed = function(id, fn) {
+    Feed.findById(id, function(err, f) {
+        if (!err || f) fn(null, f);
+        else fn('Could not find feed', null);
+    });
+};
+exports.FindFeed = findFeed;
 
-////////////////////////////////
-//   Child Feed Schema
-////////////////////////////////
+// Feed APIs
+// NOTE: user here is passed from  req.user
 
-// child feed schema
-var ChildFeedSchema = new Schema({
-	owner: String, // the owner's _id
-	created: { type : Date, default: Date.now },
-	updated: { type : Date, default: Date.now },
-	content: String, // the content object's _id
-	next: String // linked-list of other child feeds
-});
+// sample feed JSON request
+//{
+//    proj: project,
+//    content: {},
+//    tagList: []
+//}
 
-ChildFeedSchema.methods.save = function(user, content) {
-	this.created = Date.now;
-	this.owner = user._id;
-	this.content = content;
-	this.next = null;
-	this.save(function(err, cf) {
+// todo: who will ensure that the json content is sane (not html etc etc security shit)
+exports.NewFeed = function(User, json, fn) {
+    var f = new Feed({});
 
-	});
+    user.Find(User.uuid, function(err, u) {
+        json.tagList.push(json.proj); // project is also a tag!
+
+        // verify if user has permission to all projects (some tags are projects)
+        if (!err && u) user.hasModifyPermission(u, json.tagList, function(err, pu) {
+            // save the feed
+            if (!err && pu) f.Save(pu, json.content, json.tagList, function(err, nf) {
+                // update the user's stack with new content in the background
+                pu.feedStack.Push(nf);
+                fn(null, nf);
+            });
+            else fn('User does not have permission to create new feed', null);
+        });
+        else fn('Could not create new feed', null);
+    });
 };
 
-ChildFeedSchema.methods.modifyContent = function(content, fn) {
-	this.content = content;
-	this.modified = Date.now;
-	this.save(function(err, f) {
-		if (err || !f) fn('Could not modify content', null);
-		else fn(null, f);
-	});
+exports.newChildFeed = function(User, json, fn) {
+    findFeed(json.feedId, function(err, f) {
+        if (!err && f) f.AddChild(User, json.content, function(err, mf) {
+            // do some updations to all those subscribed to this main feed
+            user.Find(mf.owner, function(err, o) {
+                // this is supposed to delete any earlier references to
+                // this feed and re-add it to the user's stack
+                o.feedStack.Push(mf);
+            });
+            // push this update to all others in the access control list
+            mf.acl.forEach(function(uid) {
+                user.Find(uid, function(err, u) {
+                    if (!err && u) u.feedStack.Push(mf);
+                });
+            });
+            // okay.. done :D
+            fn(null, mf);
+        });
+        else fn('Wrong parent feed', null);
+    });
 };
-
-ChildFeedSchema.methods.delete = function(fn) {
-	this.remove(fn);
-};
-
-var ChildFeed = mongoose.model("ChildFeedSchema", ChildFeedSchema);
-
 
 ////////////////////////////////
 //   Feed Stack Schema
@@ -154,15 +219,15 @@ var ChildFeed = mongoose.model("ChildFeedSchema", ChildFeedSchema);
 // the stack of feeds constructed for each user
 // one stack per user 
 var FeedStackSchema = new Schema({
-	owner: String, // the _id of the user
-	recentStack: [FeedSchema] // the more recent and probably cached part of stack
-	archiveStack: String // the archived part of the stack
+    owner: String, // the _id of the user
+    recentStack: [FeedSchema], // the more recent and probably cached part of stack
+    archiveStack: String // the archived part of the stack
 });
 
 FeedStackSchema.methods.Push = function(feed) {
-	this.recentStack.push(feed);
-	this.CompactAndSave(); // save after compacting / archiving
+    this.recentStack.push(feed);
+    this.CompactAndSave(); // save after compacting / archiving
 };
 
-var Feed = mongoose.model("FeedStackSchema", FeedStackSchema);
-
+var FeedStack = mongoose.model("FeedStackSchema", FeedStackSchema);
+exports.FeedStack = FeedStack;
--- modules/winterfell/lib/user.js
+++ modules/winterfell/lib/user.js
@@ -5,38 +5,38 @@ var structures = require('./structures');
 var RECENT_FEEDLIST_SIZE = 40;
 
 exports.ConstructFeedsForUser = function(user, fn) {
-	// construct the query
-	var query = Feed.find({});
-	query.where('owner', user._uid);
-	query.or([{'acl': user._uid}]);
-	query.sort('timestamp', 1);
-	query.limit(RECENT_FEEDLIST_SIZE);
+    // construct the query
+    var query = Feed.find({});
+    query.where('owner', user._uid);
+    query.or([{'acl': user._uid}]);
+    query.sort('timestamp', 1);
+    query.limit(RECENT_FEEDLIST_SIZE);
 
-	// execute the query
-	query.exec(function(err, docs) {
-		if (!err || docs[0]) fn(null, docs);
-		else fn('Could not find any records', null);
-	});
+    // execute the query
+    query.exec(function(err, docs) {
+        if (!err || docs[0]) fn(null, docs);
+        else fn('Could not find any records', null);
+    });
 };
 
 exports.ConstructFeedsForUserAndTag = function(user, tag, fn) {
-	// construct the query
-	var query = Feed.find({});
-	query.where('tags', tag);
-	query.or([{'acl': user._uid, 'owner', user._uid}]);
-	query.sort('timestamp', 1);
-	query.limit(RECENT_FEEDLIST_SIZE);
+    // construct the query
+    var query = Feed.find({});
+    query.where('tags', tag);
+    query.or([{'acl': user._uid, 'owner': user._uid}]);
+    query.sort('timestamp', 1);
+    query.limit(RECENT_FEEDLIST_SIZE);
 
-	// execute the query
-	query.exec(function(err, docs) {
-		if (!err || docs[0]) fn(null, docs);
-		else fn('Could not find any records', null);
-	});
+    // execute the query
+    query.exec(function(err, docs) {
+        if (!err || docs[0]) fn(null, docs);
+        else fn('Could not find any records', null);
+    });
 };
 
 exports.ConstructFeedStackFromTag = function(tag, fn) {
-	Feed.find({'tags': tag}, function(err, docs) {
-		if (!err || docs[0]) fn(null, docs);
-		else fn(err, null);
-	});
+    Feed.find({'tags': tag}, function(err, docs) {
+        if (!err || docs[0]) fn(null, docs);
+        else fn(err, null);
+    });
 };
